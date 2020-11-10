cdea01e5a7cd68c3a6818ba1e804d84e5903c6c7
skumarsi <skumarsi@arlo.com>
2020-11-10 12:09:48 +0530

    Fix linting issues

--- algorithms/sarima/sarima.py
+++ algorithms/sarima/sarima.py
@@ -57,4 +57,4 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host= '0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', debug=True)
--- algorithms/sarima/test.py
+++ algorithms/sarima/test.py
@@ -5,9 +5,9 @@ import json
 _i = {}
 _p = {}
 d = {}
-for i in range(1,5):
-    _i[i] = random.randint(0,10)
-    _p[i] = random.randint(0,10)
+for i in range(1, 5):
+    _i[i] = random.randint(0, 10)
+    _p[i] = random.randint(0, 10)
 d["index"] = _i
 d["value"] = _p
 
