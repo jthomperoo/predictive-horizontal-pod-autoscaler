e71bbf38bead9f7256c8cce53d356e862e2dd30d
skumarsi <skumarsi@arlo.com>
2020-11-10 12:16:23 +0530

    Fix linting issues

--- algorithms/sarima/sarima.py
+++ algorithms/sarima/sarima.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from pmdarima.arima import auto_arima
 from json import JSONEncoder
 import pickle
 import numpy
 import json
 
-
+from pmdarima.arima import auto_arima
 from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 MODEL_LOCATION = '/tmp/arima.pkl'
 
@@ -22,8 +22,7 @@ class NumpyArrayEncoder(JSONEncoder):
 @app.route('/api/train', methods=['POST'])
 def train():
     content = request.json
-    data = pd.DataFrame.from_dict(content, orient='index').T.set_index('index')   
-    print(data)
+    data = pd.DataFrame.from_dict(content, orient='index').T.set_index('index')
     stepwise_model = auto_arima(data, start_p=1, start_q=1, max_p=3, max_q=3, m=12, start_P=0, seasonal=True, d=1, D=1, trace=True, error_action='ignore', suppress_warnings=True, stepwise=True)
     print(stepwise_model.aic())
     print(stepwise_model.summary())
@@ -35,8 +34,8 @@ def train():
     return jsonify('{}')
 
 def load_pred(location):
-    f = open(location, 'rb') 
-    return pickle.load(f)
+    f_d = open(location, 'rb')
+    return pickle.load(f_d)
 
 def update_model(location, data):
     model = load_pred(location)
@@ -49,10 +48,11 @@ def update_model(location, data):
 @app.route('/api/predict', methods=['POST'])
 def predict():
     content = request.json
-    data = pd.DataFrame.from_dict(content, orient='index').T.set_index('index') 
+    data = pd.DataFrame.from_dict(content, orient='index').T.set_index('index')
     model = update_model(MODEL_LOCATION, data)
     prediction, new_conf_int = model.predict(n_periods=10, return_conf_int=True)
-    return json.dumps(prediction, cls=NumpyArrayEncoder), 
+    print(new_conf_int)
+    return json.dumps(prediction, cls=NumpyArrayEncoder),
 
 
 
