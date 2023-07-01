f68184460ce6cdfdfa637d0e0e322151de8c833c
Jamie Thompson <j.thomperoo@hotmail.com>
2023-07-01 16:52:29 +0100

    Switch tuning example to use body param over query (#94)

--- examples/dynamic-holt-winters/tuning/api.py
+++ examples/dynamic-holt-winters/tuning/api.py
@@ -1,4 +1,4 @@
-# Copyright 2022 The Predictive Horizontal Pod Autoscaler Authors.
+# Copyright 2023 The Predictive Horizontal Pod Autoscaler Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 
 from flask import Flask, request
 import json
-import sys
+
 app = Flask(__name__)
 
 ALPHA_VALUE = 0.9
@@ -23,7 +23,7 @@ GAMMA_VALUE = 0.9
 
 @app.route("/holt_winters")
 def metric():
-    print("Received query parameter: {0}".format(request.args.get("value")), file=sys.stderr)
+    app.logger.info('Received context: %s', request.data)
 
     return json.dumps({
         "alpha": ALPHA_VALUE,
